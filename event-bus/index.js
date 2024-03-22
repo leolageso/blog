767663d76353ad099c7c3ca22d78d8d1be2854f2
leolageso <leonardo.lage@onegreen.com.br>
2024-03-21 21:27:45 -0300

    End of lession 37

---
+++ event-bus/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import axios from 'axios';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  axios.post("http://localhost:4000/events", event).catch(err => {
+    console.log(err);
+  });
+  axios.post("http://localhost:4001/events", event).catch(err => {
+    console.log(err);
+  });
+  axios.post("http://localhost:4002/events", event).catch(err => {
+    console.log(err);
+  });
+
+  res.send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+  console.log("Listening on port 4005");
+});
