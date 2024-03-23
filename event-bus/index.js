3e7315f920e16cb2b1a2f8c7dc23c47caedc0519
leolageso <leonardo.lage@onegreen.com.br>
2024-03-23 17:38:51 -0300

    updating services to use k8s cluester

--- event-bus/index.js
+++ event-bus/index.js
@@ -19,15 +19,15 @@ app.post("/events", (req, res) => {
   axios.post("http://posts-clusterip-srv:4000/events", event).catch(err => {
     console.log(err);
   });
-  // axios.post("http://localhost:4001/events", event).catch(err => {
-  //   console.log(err);
-  // });
-  // axios.post("http://localhost:4002/events", event).catch(err => {
-  //   console.log(err);
-  // });
-  // axios.post("http://localhost:4003/events", event).catch(err => {
-  //   console.log(err);
-  // });
+  axios.post("http://comments-clusterip-srv:4001/events", event).catch(err => {
+    console.log(err);
+  });
+  axios.post("http://query-clusterip-srv:4002/events", event).catch(err => {
+    console.log(err);
+  });
+  axios.post("http://moderation-clusterip-srv:4003/events", event).catch(err => {
+    console.log(err);
+  });
 
   res.send({ status: 'OK' });
 });
--- moderation/index.js
+++ moderation/index.js
@@ -9,7 +9,7 @@ const handleEvent = async (type, data) => {
   if (type === "CommentCreated") {
     const status = data.content.includes("orange") ? "rejected" : "approved";
 
-    await axios.post("http://localhost:4005/events", {
+    await axios.post("http://event-bus-clusterip-srv:4005/events", {
       type: "CommentModerated",
       data: {
         id: data.id,
@@ -33,7 +33,7 @@ app.listen(4003, async () => {
   console.log("Listening on port 4003");
 
   try {
-    const res = await axios.get("http://localhost:4005/events");
+    const res = await axios.get("http://event-bus-clusterip-srv:4005/events");
 
     for (let event of res.data) {
       console.log(`Processing event: ${event.type}`);
