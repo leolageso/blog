f52aabd24208dbb13f939dacfa97b4d082602ebe
leolageso <leonardo.lage@onegreen.com.br>
2024-03-23 11:31:59 -0300

    changing posts and event-bus ip to the correct clusterIP

--- posts/index.js
+++ posts/index.js
@@ -22,7 +22,7 @@ app.post("/posts", async (req, res) => {
     id, title
   };
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-clusterip-srv:4005/events", {
     type: 'PostCreated',
     data: {
       id, title
@@ -41,6 +41,5 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
-  console.log("v55");
   console.log('Listening on port 4000');
 })
