956761fe6ee925e062b6c7f1ad330510ddadd8d6
leolageso <leonardo.lage@onegreen.com.br>
2024-03-25 14:46:44 -0300

    changing cliente endpoints

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,7 +7,7 @@ const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4002/posts");
+    const res = await axios.get("http://posts.development.home:32772/posts");
 
     setPosts(res.data);
   };
